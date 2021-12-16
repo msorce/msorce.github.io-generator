3c7b6b5279e810bb3d930dcb4f17f23b9ec1cbbb
msorce <marksorce@gmail.com>
2021-12-16 12:50:09 -0800

    typescript

--- msorce.github.io/src/App.tsx
+++ msorce.github.io/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
@@ -8,8 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Dis Mark
-        <a href="/">SHEEEEEESH</a>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );

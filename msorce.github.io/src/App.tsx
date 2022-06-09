68c1275763c841e93addb9d42b1d697d97eeb6d9
Mark Sorce <msorce@users.noreply.github.com>
2022-06-08 21:34:54 -0700

    stub out navigation

--- msorce.github.io/src/App.tsx
+++ msorce.github.io/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 // import YoutubeVideo from "./features/Youtube/YoutubeVideo"\
 import VideoSection from "./features/VideoSection/VideoSection";
+import Navigation from "./features/Navigation/Navigation";
 import Hero from "./features/Hero/Hero"
 
 const App: React.FC  = () => {
   return (
     <div className="App">
+      <Navigation />
       <Hero />
       <VideoSection />
     </div>

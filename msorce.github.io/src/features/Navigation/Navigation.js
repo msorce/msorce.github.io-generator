7ae030afd8ecaf3dcb95c74c4bb762a94b408eb3
Mark Sorce <msorce@users.noreply.github.com>
2022-06-15 11:25:37 -0700

    update navbar image

--- msorce.github.io/src/features/Navigation/Navigation.js
+++ msorce.github.io/src/features/Navigation/Navigation.js
@@ -3,6 +3,8 @@ import React, { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
+import mark from "../../assets/mark.jpeg"
+
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
@@ -79,7 +81,7 @@ const Navigation = () => {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={mark}
                         alt=""
                       />
                     </Menu.Button>

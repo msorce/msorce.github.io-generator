e75d268193b3b7efb80e2642145f90c72a432ac4
Mark Sorce <msorce@users.noreply.github.com>
2022-06-16 09:07:31 -0700

    implement marquee

--- msorce.github.io/src/features/CompaniesMarquee/CompaniesMarquee.js
+++ msorce.github.io/src/features/CompaniesMarquee/CompaniesMarquee.js
@@ -1,16 +1,15 @@
 import Marquee from "react-fast-marquee";
-import google from "../../assets/google.png"
-const CompaniesMarquee = () => <Marquee speed={30}>
-<img src={google} />
-<img src={google} />
-<img src={google} />
-<img src={google} />
-<img src={google} />
-<img src={google} />
-<img src={google} />
-<img src={google} />
-</Marquee>
-
-
+import google from "../../assets/google.png";
+import disney from "../../assets/disney.png";
+const CompaniesMarquee = () => (
+    <Marquee speed={30}>
+        <img src={google} />
+				<img src={disney} />
+        <img src={google} />
+        <img src={disney} />
+        <img src={google} />
+        <img src={disney} />
+    </Marquee>
+);
 
 export default CompaniesMarquee;
